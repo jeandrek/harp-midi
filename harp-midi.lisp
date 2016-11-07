9c703180037e612fd39375b206e921627b86d453
Jonathan50 <Jonathan50@users.noreply.github.com>
2016-11-07 19:11:37 +1300

    Show note value (fix #1)

--- harp-midi.lisp
+++ harp-midi.lisp
@@ -34,7 +34,7 @@
          (load-configuration-file)
          (dolist (filename (cdr arguments))
            (cond ((probe-file filename)
-                  (format t "~a:~%" filename)
+                  (format t "~a: " filename)
                   (print-harp (read-midi-file filename)))
                  (t
                   (format t "File does not exist: ~a~%" filename)
@@ -47,52 +47,66 @@
 
 ;;;; Printing harmonica tabs
 
-(defmacro donotes ((key track) &body body)
+(defmacro donotes ((note-name midifile) &body body)
   (let ((track-name (gensym))
         (index-name (gensym))
         (message-name (gensym)))
-    `(do* ((,track-name ,track (cdr ,track-name))
+    `(do* ((,track-name (car (midifile-tracks ,midifile)) (cdr ,track-name))
            (,index-name 0 (+ ,index-name 1))
            (,message-name (car ,track-name) (car ,track-name)))
           ((null ,track-name))
-       ;; For some reason there are always two NOTE ON messages for each note
-       ;; and no NOTE OFF messages :-(
-       ;; TODO: Find out why and try to fix it.
-       (when (and (oddp ,index-name) (typep ,message-name 'note-on-message))
-         (let ((,key (message-key ,message-name)))
+       (when (and (evenp ,index-name) (typep ,message-name 'note-on-message))
+         (let ((,note-name
+                (make-note
+                 (message-key ,message-name)
+                 (round (- (message-time (cadr ,track-name)) (message-time ,message-name))
+                        (midifile-division midifile)))))
            ,@body)))))
 
-(defun print-harp (midi-file)
-  (let* ((track (car (midifile-tracks midi-file)))
-         (time-signature (find-if #'time-signature-message-p track)))
-    (format t "  ~d |" (message-numerator time-signature))
-    (donotes (note track)
+(defun print-harp (midifile)
+  (let ((time-signature (time-signature (car (midifile-tracks midifile)))))
+    (format t "~d/~d~%  " (message-numerator time-signature)
+            (expt 2 (message-denominator time-signature)))
+    (donotes (note midifile)
       (let ((hole (note->hole note)))
         (if hole
             (format t "~2d" hole)
             (return-from print-harp))))
-    (format t "~%  ~d |" (expt 2 (message-denominator time-signature)))
-    (donotes (note track)
+    (format t "~%  ")
+    (donotes (note midifile)
       (cond ((blowp note) (princ " ↑"))
             ((drawp note) (princ " ↓"))))
+    (format t "~%  ")
+    (donotes (note midifile)
+      (format t "~2d" (duration note)))
     (format t "~%~%")))
 
 (defun time-signature-message-p (message)
   (typep message 'time-signature-message))
 
+(defun time-signature (track)
+  (find-if #'(lambda (message) (typep message 'time-signature-message)) track))
+
+;;;; Notes
+
+(defun make-note (pitch duration) (cons pitch duration))
+(defun pitch (note) (car note))
+(defun duration (note) (cdr note))
+
 ;;;; Converting MIDI notes to holes and directions
 
 (defun note->hole (note)
-  (cond ((< note 48) (format t "~%Note too low: ~d~%" note))
-        ((> note 84) (format t "~%Note too high: ~d~%" note))
-        (t (aref #(1 nil 1 nil 2 2 nil 3 nil nil nil 3
-                   4 nil 4 nil 5 5 nil 6 nil 6 nil 7
-                   7 nil 8 nil 8 9 nil 9 nil 10 nil nil
-                   10)
-                 (- note 48)))))
+  (let ((pitch (pitch note)))
+    (cond ((< pitch 48) (format t "~%Note too low: ~d~%" pitch))
+          ((> pitch 84) (format t "~%Note too high: ~d~%" pitch))
+          (t (aref #(1 nil 1 nil 2 2 nil 3 nil nil nil 3
+                     4 nil 4 nil 5 5 nil 6 nil 6 nil 7
+                     7 nil 8 nil 8 9 nil 9 nil 10 nil nil
+                     10)
+                   (- pitch 48))))))
 
 (defun blowp (note)
   (aref #(t nil nil nil t nil nil t nil nil nil nil)
-        (mod note 12)))
+        (mod (pitch note) 12)))
 
 (defun drawp (note) (not (blowp note)))
